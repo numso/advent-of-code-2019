83f9988fc64a484ce8516c06b8dfa8a581f556f5
Dallin Osmun <dosmun@gmail.com>
2019-12-12 20:38:54 -0700

    day12

--- lib/day12.ex
+++ lib/day12.ex
@@ -1,72 +1,73 @@
 defmodule Day12 do
+  @input [{-3, 15, -11}, {3, 13, -19}, {-13, 18, -2}, {6, 0, -1}]
+  def test1, do: [{-1, 0, 2}, {2, -10, -7}, {4, -8, 8}, {3, 5, -1}]
+  def test2, do: [{-8, -10, 0}, {5, 5, 10}, {2, -7, 3}, {9, -8, -3}]
+
   @doc """
   iex> Day12.part1()
   12070
   """
-  def part1 do
-    [
-      {-3, 15, -11},
-      {3, 13, -19},
-      {-13, 18, -2},
-      {6, 0, -1}
-    ]
-    |> prep()
-    |> simulate(1000)
-    |> energy()
-  end
+  def part1(), do: simulate_complex(@input, 1000)
 
   @doc """
-  iex> [{-1, 0, 2}, {2, -10, -7}, {4, -8, 8}, {3, 5, -1}] |> Day12.prep() |> Day12.simulate(10) |> Day12.energy()
+  iex> Day12.part2()
+  500903629351944
+  """
+  def part2(), do: find_repeat_complex(@input)
+
+  @doc """
+  iex> Day12.simulate_complex(Day12.test1, 10)
   179
 
-  iex> [{-8, -10, 0}, {5, 5, 10}, {2, -7, 3}, {9, -8, -3}] |> Day12.prep() |> Day12.simulate(100) |> Day12.energy()
+  iex> Day12.simulate_complex(Day12.test2, 100)
   1940
   """
-  def energy(moons) do
-    Enum.map(moons, fn {{x, y, z}, {dx, dy, dz}} ->
-      (abs(x) + abs(y) + abs(z)) * (abs(dx) + abs(dy) + abs(dz))
-    end)
+  def simulate_complex(moons, count) do
+    0..2
+    |> Enum.map(&filter_axis(moons, &1))
+    |> Enum.map(&simulate(&1, count))
+    |> Enum.zip()
+    |> Enum.map(fn {{x, dx}, {y, dy}, {z, dz}} -> sum(x, y, z) * sum(dx, dy, dz) end)
     |> Enum.sum()
   end
 
-  @doc """
-  iex> [{-1, 0, 2}, {2, -10, -7}, {4, -8, 8}, {3, 5, -1}] |> Day12.prep() |> Day12.simulate(10)
-  [
-    {{2, 1, -3}, {-3, -2, 1}},
-    {{1, -8, 0}, {-1, 1, 3}},
-    {{3, -6, 1}, {3, 2, -3}},
-    {{2, 0, 4}, {1, -1, -1}}
-  ]
-
-  iex> [{-8, -10, 0}, {5, 5, 10}, {2, -7, 3}, {9, -8, -3}] |> Day12.prep() |> Day12.simulate(100)
-  [
-    {{8, -12, -9}, {-7, 3, 0}},
-    {{13, 16, -3}, {3, -11, -5}},
-    {{-29, -11, -1}, {-3, 7, 4}},
-    {{16, -13, 23}, {7, 1, 1}}
-  ]
-  """
   def simulate(moons, 0), do: moons
+  def simulate(moons, count), do: tick(moons) |> simulate(count - 1)
 
-  def simulate(moons, iteration) do
-    Enum.map(moons, fn {{x, y, z} = pos, vel} ->
-      new_vel =
-        Enum.reduce(moons, vel, fn {{x1, y1, z1}, _}, {dx, dy, dz} ->
-          dx1 = get_modifier(x, x1)
-          dy1 = get_modifier(y, y1)
-          dz1 = get_modifier(z, z1)
-          {dx + dx1, dy + dy1, dz + dz1}
-        end)
+  defp sum(a, b, c), do: abs(a) + abs(b) + abs(c)
 
-      {pos, new_vel}
-    end)
-    |> Enum.map(fn {{x, y, z}, {dx, dy, dz} = vel} -> {{x + dx, y + dy, z + dz}, vel} end)
-    |> simulate(iteration - 1)
+  @doc """
+  iex> Day12.find_repeat_complex(Day12.test1)
+  2772
+
+  iex> Day12.find_repeat_complex(Day12.test2)
+  4686774924
+  """
+  def find_repeat_complex(moons) do
+    0..2
+    |> Enum.map(&filter_axis(moons, &1))
+    |> Enum.map(&find_repeat/1)
+    |> lcm()
   end
 
-  def prep(moons), do: Enum.map(moons, &{&1, {0, 0, 0}})
+  def find_repeat(moons), do: find_repeat(moons, moons, 0)
+  def find_repeat(moons, moons, count) when count != 0, do: count
+  def find_repeat(moons, start, count), do: tick(moons) |> find_repeat(start, count + 1)
 
-  defp get_modifier(a, b) when a == b, do: 0
-  defp get_modifier(a, b) when a < b, do: 1
-  defp get_modifier(_, _), do: -1
+  defp lcm(a, b), do: floor(a * b / Integer.gcd(a, b))
+  defp lcm(arr), do: Enum.reduce(arr, &lcm/2)
+
+  defp filter_axis(moons, i), do: Enum.map(moons, &{elem(&1, i), 0})
+
+  def tick(moons) do
+    next_vel = fn pos -> Enum.map(moons, &dir(pos, &1)) |> Enum.sum() end
+
+    moons
+    |> Enum.map(fn {pos, vel} -> {pos, vel + next_vel.(pos)} end)
+    |> Enum.map(fn {pos, vel} -> {pos + vel, vel} end)
+  end
+
+  defp dir(a, {b, _}) when a == b, do: 0
+  defp dir(a, {b, _}) when a < b, do: 1
+  defp dir(_, _), do: -1
 end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -10,4 +10,5 @@ defmodule AocTest do
   doctest Day9
   doctest Day10
   doctest Day11
+  doctest Day12
 end
